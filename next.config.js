44bf0657fb112c5a2d4982833d0b4795f65d093f
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-07-16 17:35:48 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+// disable eslint
+// disable type check
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+}
+
 
 module.exports = nextConfig
